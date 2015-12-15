e730f2cbe8481d9a0b0e953996b1c80c3a8e4a2c
unknown <bclaringbold@GC-JXCYY52.admin.georgianc.on.ca>
2015-12-15 14:43:56 -0500

    update schema

--- server/models/survey.js
+++ server/models/survey.js
@@ -11,8 +11,32 @@ var surveySchema = new Schema({
 	completed: Boolean,
 	username: String,
 	note: String,
+	questions: String,
+	question01: String,
+	question02: String,
+	question03: String,
+	question04: String,
+	question05: String,
+	question06: String,
+	question07: String,
+	question08: String,
+	question09: String,
+	question10: String,
+	answerq01: String,
+	answerq02: String,
+	answerq03: String,
+	answerq04: String,
+	answerq05: String,
+	answerq06: String,
+	answerq07: String,
+	answerq08: String,
+	answerq09: String,
+	answerq10: String,
 	salt: String,
-	updated: {type:Date, default: Date.Now}
+	updated: {type:Date, default: Date.Now},
+	created: {type:Date},
+	startdate: {type: Date},
+	expirydate: {type: Date} 
 }, {
 	collection: 'survey'
 });
